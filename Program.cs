2286dfaf630cc0b6ea48ccd0e7b7e8154c00ac65
George Breje <george.breje2@gmail.com>
2020-10-05 19:05:55 +0300

    Incercare Program.cs 2

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace CCC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            numeColegi nume = new numeColegi();
+            foreach(string i in nume.numeLista)
+                Console.WriteLine(i);
+
+        }
+    }
+
+    public class numeColegi
+    {   
+        public string[] numeLista = {"Andrei","Pavel","Petru"};
+
+    }
+    //comentariu
+}
